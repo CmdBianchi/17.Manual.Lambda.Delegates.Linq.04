0b375c074178207a605a5243dd862a6e52e637f9
marcu <marcu@LAPTOP-MTO8J5A0>
2021-07-23 17:39:08 -0300

    Adicionar arquivos de projeto.

---
+++ 17.Manual.Lambda.Delegates.Linq.04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Services;
+namespace _17.Manual.Lambda.Delegates.Linq._04 {
+    class Program {
+        delegate double BinaryNumericOperation(double n1, double n2);
+        static void Main(string[] args) {
+            double a = 10;
+            double b = 12;
+
+            // BinaryNumericOperation op = CalculationService.Sum;
+            BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+
+            // double result = op(a, b);
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
